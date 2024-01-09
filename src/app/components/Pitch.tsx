a5b416a7690078a2a76c3643cb947aa8e7c6b450
marcal33 <marcoff033@gmail.com>
2024-01-09 18:41:09 +0100

    placeholder pitch SVG and player icons

--- src/app/components/Pitch.tsx
+++ src/app/components/Pitch.tsx
@@ -2,10 +2,112 @@ import React from "react";
 import Image from "next/image";
 import pitchSvg from "@/app/public/pitch.svg";
 
-export default function Pitch() {
+//  /<img src={prova} alt="" className="rounded-lg" />
+export default function Pitch({ prova }: { prova: string }) {
   return (
-    <div>
-      <Image alt="Pitch" src={pitchSvg} width={500} height={500} />
+    <div className="self-center">
+      <div className="relative">
+        <Image alt="Pitch" src={pitchSvg} width={500} height={500} />
+        {/* defense, RB*/}
+        <div className="h-[80px] w-[70px] z-50 absolute top-[31rem] left-[24.5rem]">
+          <Image
+            alt="test"
+            src={prova}
+            fill
+            className="border border-black rounded-md shadow-xl shadow-zinc-900"
+          />
+        </div>
+        {/* defense, Left CB*/}
+        <div className="h-[80px] w-[70px] z-50 absolute top-[33rem] left-[10rem]">
+          <Image
+            alt="test"
+            src={prova}
+            fill
+            className="border border-black rounded-md shadow-xl shadow-zinc-900"
+          />
+        </div>
+        {/* defense, Right CB*/}
+        <div className="h-[80px] w-[70px] z-50 absolute top-[33rem] left-[17rem]">
+          <Image
+            alt="test"
+            src={prova}
+            fill
+            className="border border-black rounded-md shadow-xl shadow-zinc-900"
+          />
+        </div>
+        {/* defense, LB*/}
+        <div className="h-[80px] w-[70px] z-50 absolute top-[31rem] left-10">
+          <Image
+            alt="test"
+            src={prova}
+            fill
+            className="border border-black rounded-md shadow-xl shadow-zinc-900"
+          />
+        </div>
+        {/* Midfielders, Left CM*/}
+        <div className="h-[80px] w-[70px] z-50 absolute top-[20rem] left-[7rem]">
+          <Image
+            alt="test"
+            src={prova}
+            fill
+            className="border border-black rounded-md shadow-xl shadow-zinc-900"
+          />
+        </div>
+        {/* Midfielders, Right CM*/}
+        <div className="h-[80px] w-[70px] bg-white z-50 absolute top-[20rem] left-[20rem]">
+          <Image
+            alt="test"
+            src={prova}
+            fill
+            className="border border-black rounded-md shadow-xl shadow-zinc-900"
+          />
+        </div>
+        {/* Midfielders, DMF*/}
+        <div className="h-[80px] w-[70px] bg-blue-500 z-50 absolute top-[25rem] left-[13.5rem]">
+          <Image
+            alt="test"
+            src={prova}
+            fill
+            className="border border-black rounded-md shadow-xl shadow-zinc-900"
+          />
+        </div>
+        {/* Attackers, CF*/}
+        <div className="h-[80px] w-[70px] bg-black z-50 absolute top-[5rem] left-[13.5rem]">
+          <Image
+            alt="test"
+            src={prova}
+            fill
+            className="border border-black rounded-md shadow-xl shadow-zinc-900"
+          />
+        </div>
+        {/* Attackers, LWF*/}
+        <div className="h-[80px] w-[70px] bg-green-200 z-50 absolute top-[10rem] left-[4rem]">
+          <Image
+            alt="test"
+            src={prova}
+            fill
+            className="border border-black rounded-md shadow-xl shadow-zinc-900"
+          />
+        </div>
+        {/* Attackers, RWF*/}
+        <div className="h-[80px] w-[70px] bg-black z-50 absolute top-[10rem] left-[23rem]">
+          <Image
+            alt="test"
+            src={prova}
+            fill
+            className="border border-black rounded-md shadow-xl shadow-zinc-900"
+          />
+        </div>
+        {/* Goalkeeper */}
+        <div className="h-[80px] w-[70px] bg-black z-50 absolute top-[41rem] left-[13.5rem]">
+          <Image
+            alt="test"
+            src={prova}
+            fill
+            className="border border-black rounded-md shadow-xl shadow-zinc-900"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,23 @@ import PlayerCard from "./components/PlayerCard";
 import { TeamComp } from "./types/playerDb";
 import { randomRolePicks } from "./utils/randomRolePicks";
 import { fetchPlayerData } from "./utils/fetchPlayerData";
-import Image from "next/image";
-import pitchSvg from "@/app/public/pitch.svg";
 import Pitch from "./components/Pitch";
 
 export default async function Home() {
   const players: TeamComp = db;
 
   const CF = randomRolePicks(players, "CF");
+  const data = await fetchPlayerData(3333, "en");
+  const testpic = data.playerProfile.playerImage;
 
   return (
-    <div className="bg-red-500 flex flex-col justify-between">
-      {CF.map((player) => (
+    <div className="bg-red-500 flex flex-col justify-center">
+      <Pitch prova={testpic} />
+    </div>
+  );
+}
+{
+  /* {CF.map((player) => (
         <PlayerCard
           lang="en"
           playerId={player}
@@ -22,7 +27,5 @@ export default async function Home() {
           key={player}
         />
       ))}
-      <Pitch />
-    </div>
-  );
+      */
 }
